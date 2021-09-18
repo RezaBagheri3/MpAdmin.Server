30f9ccc029e4de14ed1845f279da30918a7c1453
mearajrajabi <73219893+mearajrajabi@users.noreply.github.com>
2021-09-18 15:26:06 +0430

    done commit bot

--- MpAdmin.Server/MpAdmin.Server/Controllers/LoginController.cs
+++ MpAdmin.Server/MpAdmin.Server/Controllers/LoginController.cs
@@ -1,69 +1,3 @@
-<<<<<<< HEAD
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using MpAdmin.Server.DAL.Context;
-using MpAdmin.Server.Domain;
-using MpAdmin.Server.Models;
-
-namespace MpAdmin.Server.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class LoginController : ControllerBase
-    {
-        private MpAdminContext _context;
-
-        public LoginController(MpAdminContext context)
-        {
-            _context = context;
-        }
-
-        [HttpPost]
-        public async Task<ActionResult<int>> CheckLoginInfo([FromBody] LoginInfoModel model)
-        {
-            try
-            {
-                UnitOfWork unitOfWork = new UnitOfWork(_context);
-
-                var item = await unitOfWork.UserRepo.FirstOrDefaultAsync(r => r.UserName == model.UserName && r.Password == model.Password);
-
-                if (item != null)
-                {
-                    return Ok(
-                        new
-                        {
-                            result = 1
-                        }
-                    );
-                }
-                else
-                {
-                    return Ok(
-                        new
-                        {
-                            result = 2
-                        }
-                    );
-                }
-
-            }
-            catch (Exception e)
-            {
-                return BadRequest(
-                    new
-                    {
-                        e
-                    }
-                );
-            }
-        }
-    }
-}
-=======
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -129,4 +63,3 @@ namespace MpAdmin.Server.Controllers
         }
     }
 }
->>>>>>> d0ddf00aa9f1e22bdadb5df13636f9514f61fd58
