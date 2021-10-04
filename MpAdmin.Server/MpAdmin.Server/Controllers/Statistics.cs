11a6cb4cc9acc2e386fcd2a80f34f3f61401b3f8
Reza <rezabagheri3831@gmail.com>
2021-10-04 20:12:57 +0330

    Add Statistics Controller & WallPaper-Bot Statistics

---
+++ MpAdmin.Server/MpAdmin.Server/Controllers/Statistics.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MpAdmin.Server.DAL.Context;
+using MpAdmin.Server.Domain;
+
+namespace MpAdmin.Server.Controllers
+{
+    [Route("api/[controller]/")]
+    [ApiController]
+    public class Statistics : ControllerBase
+    {
+        private MpAdminContext _context;
+
+        public Statistics(MpAdminContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<ActionResult<int>> WallPaperStatistics()
+        {
+            try
+            {
+                UnitOfWork unitOfWork = new UnitOfWork(_context);
+
+                var wallPaperCodeCount = unitOfWork.WallPaperRepo.Get().Count();
+
+                var totalWallPaperStock = await unitOfWork.WallPaperRepo.GetAsync().Result.Select(r => r.Stock).SumAsync();
+
+                var totalWallPapersPrice = await unitOfWork.WallPaperRepo.GetAsync().Result.Select(p => p.TotalPrice).SumAsync();
+
+                return Ok(
+                    new
+                    {
+                        wallPaperCodeCount,
+                        totalWallPaperStock,
+                        totalWallPapersPrice
+                    }
+                );
+            }
+            catch (Exception e)
+            {
+                return BadRequest(
+                    new
+                    {
+                        e
+                    }
+                );
+            }
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<ActionResult<int>> TelegramBotStatistics()
+        {
+            try
+            {
+                UnitOfWork unitOfWork = new UnitOfWork(_context);
+
+                var botUserCount = unitOfWork.TelegramUserRepo.Get().Count();
+
+                return Ok(
+                    new
+                    {
+                        botUserCount
+                    }
+                );
+            }
+            catch (Exception e)
+            {
+                return BadRequest(
+                    new
+                    {
+                        e
+                    }
+                );
+            }
+        }
+    }
+}
